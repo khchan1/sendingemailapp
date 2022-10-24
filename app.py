eb3f4b659f640595e02a65f4788edd70b968544d
Billy Chan <nf1bhc@mmgoverseas.federated.fds>
2022-10-24 14:11:33 +0800

    1.0

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from flask_mail import Mail,Message # pip install flask_mail
 import requests
@@ -6,22 +7,22 @@ app = Flask(__name__) #Initialise app
 
 
 # Config
-app.config['MAIL_SERVER'] = "smtp.mailgun.org"
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'postmaster@sandbox8706cdc9df464716bd0a18de0fa513e2.mailgun.org'
-app.config['MAIL_PASSWORD'] = 'ad506e5a52225d602b0c0e3cb4fc5285-8845d1b1-7aa456ad'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = False
-mail = Mail(app) # Instance
+MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
+MAILGUN_PUBLIC_KEY =  os.environ.get("MAILGUN_PUBLIC_KEY")
+MAILGUN_DOMAIN =  os.environ.get("MAILGUN_DOMAIN")
+MAILGUN_SMTP_LOGIN = os.environ.get("MAILGUN_SMTP_LOGIN")
+MAILGUN_SMTP_PASSWORD = os.environ.get("MAILGUN_SMTP_PASSWORD")
+MAILGUN_SMTP_PORT = os.environ.get("MAILGUN_SMTP_PORT")
+MAILGUN_SMTP_SERVER = os.environ.get("MAILGUN_SMTP_SERVER")
 
 def send_simple_message():
 	return requests.post(
-		"https://api.mailgun.net/v3/postmaster@sandbox8706cdc9df464716bd0a18de0fa513e2.mailgun.org/messages",
-		auth=("api", "5f143565005ce4f6d999f47f42de8593-8845d1b1-88c9d249"),
+		"https://api.mailgun.net/v3/" + str(MAILGUN_DOMAIN) + "/messages",
+		auth=("api", MAILGUN_API_KEY),
 		data={"from": "billy.chan@macys.com",
 			"to": ["billy.chan@macys.com", "billy.chan@macys.com"],
-			"subject": "Hello",
-			"text": "Testing some Mailgun awesomness!"})
+			"subject": "Sending function - Testing",
+			"text": "Testing 123"})
 
 
 
