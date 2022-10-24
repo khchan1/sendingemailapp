50d1aa0c32b629dd3d4e099c5c6e7e271dea3ec7
Billy Chan <nf1bhc@mmgoverseas.federated.fds>
2022-10-24 15:15:51 +0800

    1.0

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import os
 from flask import Flask, render_template, request
 from flask_mail import Mail,Message # pip install flask_mail
 import requests
+import smtplib, ssl
 
 app = Flask(__name__) #Initialise app
 
 
-# Config
+# Config - Mailgun
 MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
 MAILGUN_PUBLIC_KEY =  os.environ.get("MAILGUN_PUBLIC_KEY")
 MAILGUN_DOMAIN =  os.environ.get("MAILGUN_DOMAIN")
@@ -15,7 +16,9 @@ MAILGUN_SMTP_PASSWORD = os.environ.get("MAILGUN_SMTP_PASSWORD")
 MAILGUN_SMTP_PORT = os.environ.get("MAILGUN_SMTP_PORT")
 MAILGUN_SMTP_SERVER = os.environ.get("MAILGUN_SMTP_SERVER")
 
-def send_simple_message(email, subject, message):
+
+# Mailgun Send Email Function
+def send_simple_message_mailgun(email, subject, message):
 	return requests.post(
 		"https://api.mailgun.net/v3/" + str(MAILGUN_DOMAIN) + "/messages",
 		auth=("api", MAILGUN_API_KEY),
@@ -23,6 +26,32 @@ def send_simple_message(email, subject, message):
 			"to": [email],
 			"subject": subject,
 			"text": message})
+
+# Config - Cloudmailin
+CLOUDMAILIN_FORWARD_ADDRESS = os.environ.get("CLOUDMAILIN_FORWARD_ADDRESS")
+CLOUDMAILIN_PASSWORD =  os.environ.get("CLOUDMAILIN_SEND_PASSWORD")
+CLOUDMAILIN_SECRET =  os.environ.get("CLOUDMAILIN_SECRET")
+CLOUDMAILIN_USERNAME =  os.environ.get("CLOUDMAILIN_SEND_USERNAME")
+
+# Cloudmailin
+def send_simple_message_cloud():
+    hostname = "smtp.cloudmta.net"
+    username = CLOUDMAILIN_USERNAME
+    password = CLOUDMAILIN_PASSWORD
+
+    message = ("""\
+Subject: Test from Python
+To: herokutext@gmail.com
+From: from@example.com
+
+This message is sent from Python.""")
+
+    server = smtplib.SMTP(hostname, 587)
+    server.ehlo() # Can be omitted
+    server.starttls(context=ssl.create_default_context()) # Secure the connection
+    server.login(username, password)
+    server.sendmail("from@example.com", "herokutext@gmail.com", message)
+    server.quit
 
 
 
@@ -36,7 +65,7 @@ def result():
         #msg = Message(subject, sender = 'billy.chan@macys.com' , recipients = [email])
         #msg.body = message
         #mail.send(msg)
-        send_simple_message(email, subject, message)
+        send_simple_message_cloud()
         return render_template('index.html', sent = True)
     else:
         return render_template('index.html')
